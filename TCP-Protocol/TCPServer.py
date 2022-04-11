c829cc40fd53fdb4d6611b5c40565d307f09057a
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-04-11 08:19:38 +0300

    Update TCPServer.py

--- TCP-Protocol/TCPServer.py
+++ TCP-Protocol/TCPServer.py
@@ -1,14 +1,15 @@
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM) #Sock_Stream = ορίζουμε ότι θα είναι socket tcp
+serverSocket = socket(AF_INET, SOCK_STREAM) #Sock_Stream = we set that we will work with a tcp socket.
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1) #μπορώ να λάβω και να κρατήσω σε μια ουρά μέχρι 1 αίτημα tcp
+serverSocket.listen(1) #We can receive and keep in a queue up to 1 tcp request.
+
 print('The server is ready to receive')
 while True:
-    connectionSocket, addr = serverSocket.accept() #περιμένει να αποδεχθεί το αίτημα για σύνδεση.Δημιουργεί μια νέα socket που είναι το άκρο της εξυπηρέτησης για τη συγκεκριμένη σύνδεση.
+    connectionSocket, addr = serverSocket.accept() #Waiting to accept the request.
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
     connectionSocket.close()
 
-#Όταν επιχειρήσω νέα σύνδεση θα δημιουργηθεί εξαρχής καινούργιο connectionSocket.
+#When we try to establish a new connection, a new connectionSocket will be created.
