7371acc2b79247872aa66f63b80e84a2f5071b91
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-04-11 08:29:05 +0300

    Delete UDP-with-more- reqs-and-answers directory

--- UDP-with-more- reqs-and-answers/UDPImpostor.py
+++
@@ -1,7 +0,0 @@
-from socket import *
-ClientName = 'localhost'
-ClientPort = 33316 #Το Port το βρήσκουμε μέσω του UDPClient.
-message = 'boo'
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.sendto(message.encode(), (ClientName, ClientPort))
-clientSocket.close()
