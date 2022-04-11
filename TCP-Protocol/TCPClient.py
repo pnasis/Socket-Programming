8ca3c3da086276303766c23b8ef0c14330acb1c3
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-04-11 08:20:20 +0300

    Update TCPClient.py

--- TCP-Protocol/TCPClient.py
+++ TCP-Protocol/TCPClient.py
@@ -3,6 +3,7 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
 sentence = input('Input lowercase sentence: ')
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
